7cf22b6743219869323084f709e39e63c919b956
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2023-10-03 19:19:17 +0300

    Routing Finish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {NavBar} from "./components/NavBar/NavBar.js";
 import {About} from "./components/About/About.js";
 import {Projects} from "./components/Projects/Projects.js";
 import {Footer} from "./components/Footer/Footer.js";
-import {Hero} from "./components/Hero/Hero.js";
 import {Main} from "./components/Main/Main";
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
             <Route path="/" element={<Main />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Footer />} />
+            <Route path="/contact" element={<></>} />
         </Routes>
         <Footer/>
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,10 @@ export const NavBar = () => {
     return (
         <nav id="top-menu" className={styles.topMenu}>
             <ul className={styles.topMenu__list}>
-                <li className={styles.topMenu__list__menu}><Link to="/">Home</Link></li>
-                <li className={styles.topMenu__list__menu}><Link to="/about">About</Link></li>
-                <li className={styles.topMenu__list__menu}><Link to="/projects">Projects</Link></li>
-                <li className={styles.topMenu__list__menu}><Link to="/contact">Contact</Link></li>
+                <li className={styles.topMenu__list__menu}><Link to="/" className={styles.topMenu__list__menu__link}>Home</Link></li>
+                <li className={styles.topMenu__list__menu}><Link to="/about" className={styles.topMenu__list__menu__link}>About</Link></li>
+                <li className={styles.topMenu__list__menu}><Link to="/projects" className={styles.topMenu__list__menu__link}>Projects</Link></li>
+                <li className={styles.topMenu__list__menu}><Link to="/contact" className={styles.topMenu__list__menu__link}>Contact</Link></li>
             </ul>
         </nav>
     )
